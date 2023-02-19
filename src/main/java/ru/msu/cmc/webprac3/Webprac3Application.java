660417495bd379423a0a32c8430157b23bad3a6a
pbedrin <pbedrin@mail.ru>
2023-02-19 12:28:26 +0300

    Init Gradle

---
+++ src/main/java/ru/msu/cmc/webprac3/Webprac3Application.java
@@ -0,0 +1,13 @@
+package ru.msu.cmc.webprac3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webprac3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webprac3Application.class, args);
+	}
+
+}
